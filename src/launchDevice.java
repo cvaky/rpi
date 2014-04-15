41e990a2065776dda759e7eff025cfac2e599aca
cvaky <info@cvaky.sk>
2014-04-15 20:36:08 +0200

    first commit

---
+++ src/launchDevice.java
@@ -0,0 +1,19 @@
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+
+class launchDevice implements Runnable {
+
+	Thread runner;
+	String idTag;
+
+	public launchDevice() {
+	}
+
+
+	public void run() {
+		
+	}
+
+}
